582169c43a1e95c0031d892fde2aa91ee7acba73
Jesse S <boneitis@users.noreply.github.com>
2021-07-21 00:33:25 -0700

    Create hitme.sh

---
+++ hitme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~
+wget https://github.com/boneitis/scratchpad/raw/master/.vimrc
+https://github.com/boneitis/scratchpad/raw/master/.tmux.conf
+mkdir .vim
+cd .vim
+mkdir colors
+cd colors
+wget https://github.com/boneitis/oceanic-material/raw/master/colors/oceanic_material.vim
